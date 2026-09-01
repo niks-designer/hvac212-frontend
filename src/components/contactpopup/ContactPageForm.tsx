7699ee46175ffdf39a7d4a9311dd6b9ea88ef385
niks-designer <solidred108@gmail.com>
2026-09-01 22:54:57 +0530

    Update the note on contact us page

--- src/components/contactpopup/ContactPageForm.tsx
+++ src/components/contactpopup/ContactPageForm.tsx
@@ -428,10 +428,10 @@ export default function ContactPageForm() {
             </div>
 
             <p className="mx-auto mt-5 max-w-180 text-center text-[15px] leading-5">
-                By submitting, you agree to receive emails and texts from
-                Triangles PH LLC. Message & data rates may apply. Frequency
-                varies. Reply STOP to opt out of texts. Unsubscribe links
-                included in emails.
+                By submitting, you agree to receive emails and texts from 212
+                HVAC LLC. Message & data rates may apply. Frequency varies.
+                Reply STOP to opt out of texts. Unsubscribe links included in
+                emails.
             </p>
         </form>
     );
